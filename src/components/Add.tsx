e9e45c528e09e47f039055c06e06d7ca38f7e215
Kulcsar Mate <kulcsarmate@gmail.com>
2022-06-22 09:35:53 +0200

    style: :lipstick: styling

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,5 +1,5 @@
 import { ChangeEventHandler } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Card } from "react-bootstrap";
 import { TodoDay, Days } from "../model";
 
 interface AddProps {
@@ -19,39 +19,44 @@ const Add = ({
 }: AddProps) => {
   return (
     <div className="row mt-4">
-      <Form.Group className="mb-3 d-flex align-items-center justify-content-around">
-        <Form.Control
-          placeholder="Describe task"
-          type="text"
-          name="inputTodo"
-          value={inputTodoValue}
-          onChange={onInputChange}
-          className="w-50"
-        />
-        <Form.Check
-          inline
-          type="radio"
-          id="today"
-          label="Today"
-          value="today"
-          name="day"
-          onChange={onRadioChange}
-          checked={inputDayValue === Days.TODAY}
-        />
-        <Form.Check
-          inline
-          type="radio"
-          id="tomorrow"
-          label="Tomorrow"
-          value="tomorrow"
-          name="day"
-          onChange={onRadioChange}
-          checked={inputDayValue === Days.TOMORROW}
-        />
-        <Button variant="success" onClick={onButtonClick}>
-          Save
-        </Button>
-      </Form.Group>
+      <Card>
+        <Card.Body>
+          <Card.Title>Add new task</Card.Title>
+          <Form.Group className="mb-3 d-flex align-items-center justify-content-around">
+            <Form.Control
+              placeholder="Describe task"
+              type="text"
+              name="inputTodo"
+              value={inputTodoValue}
+              onChange={onInputChange}
+              className="w-50"
+            />
+            <Form.Check
+              inline
+              type="radio"
+              id="today"
+              label="Today"
+              value="today"
+              name="day"
+              onChange={onRadioChange}
+              checked={inputDayValue === Days.TODAY}
+            />
+            <Form.Check
+              inline
+              type="radio"
+              id="tomorrow"
+              label="Tomorrow"
+              value="tomorrow"
+              name="day"
+              onChange={onRadioChange}
+              checked={inputDayValue === Days.TOMORROW}
+            />
+            <Button variant="success" onClick={onButtonClick}>
+              Save
+            </Button>
+          </Form.Group>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
